e9226274bf86f4cb80cd8f8c1ef98d7c8e1f4407
sandbenders <fabrizio@fabriziopoltronieri.com>
2018-04-27 19:54:53 +0100

    database reorganized

--- Database.py
+++ Database.py
@@ -7,8 +7,8 @@ class Database:
     def __init__(self):
         super().__init__()
         self.client = MongoClient()
-        self.db = self.client.goodreads_quotes
-        self.collection = self.db.love
+        self.db = self.client.love_apparatus
+        self.collection_sentences = self.db.love_sentences
 
     def get_love_sentence(self):
         pipeline = [
@@ -16,19 +16,7 @@ class Database:
             {"$sample": {"size": 1}}
         ]
 
-        sentence = list(self.collection.aggregate(pipeline))[0]
+        sentence = list(self.collection_sentences.aggregate(pipeline))[0]
 
         return sentence['text']
 
-
-# def thread_func():
-#     print("Thread works")
-#     timer = QtCore.QTimer()
-#     timer.timeout.connect(timer_func)
-#     timer.start(1000)
-#     print(timer.remainingTime())
-#     print(timer.isActive())
-#     timers.append(timer)
-#
-# def timer_func():
-#     print("Timer works")
