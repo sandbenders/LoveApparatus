f15583722738c912f4ccdb204d4a57ceb74d7d0c
sandbenders <fabrizio@fabriziopoltronieri.com>
2018-04-28 17:54:06 +0100

    text file to train the network cleaned and exported

--- IdentifyBadWords.py
+++ IdentifyBadWords.py
@@ -4,11 +4,11 @@ import nltk
 
 
 class IdentifyBadWords:
-    def __init__(self):
+    def __init__(self, list_bad_words_file='listBadWords/full-list-of-bad-words-utf-8.txt'):
         super().__init__()
 
         # load list of bad words
-        self.list_of_bad_words = set(line.strip() for line in open('listBadWords/full-list-of-bad-words-utf-8.txt'))
+        self.list_of_bad_words = set(line.strip() for line in open(list_bad_words_file))
 
     def has_bad_words(self, sentence):
         return self.list_of_bad_words.intersection(nltk.word_tokenize(sentence))
---
+++ trainNeuralNetwork/exportFileFromMongoDB/export_mongo_db_txt_file.py
@@ -0,0 +1,33 @@
+# awk '!seen[$0]++' filename
+
+import pymongo
+from IdentifyBadWords import *
+
+MONGO_URI = 'mongodb://localhost:27017'
+MONGO_DATABASE = 'love_apparatus'
+COLLECTION_NAME = 'love_sentences'
+
+client = pymongo.MongoClient(MONGO_URI)
+db = client[MONGO_DATABASE]
+
+bad_words = IdentifyBadWords('../../listBadWords/full-list-of-bad-words-utf-8.txt')
+
+pipeline = [
+            {'$project': {'_id': 0, 'text': 1}},
+            {'$redact': {'$cond': [{'$lte': [{'$strLenCP': '$text'}, 140]}, '$$KEEP', '$$PRUNE']}}
+        ]
+
+texts = list(db[COLLECTION_NAME].aggregate(pipeline))
+
+for text in texts:
+    only_text = text['text']
+    if not bad_words.has_bad_words(only_text):
+        with open('loveQuotes.txt', 'ab') as output_file:
+            only_text = only_text.replace('...', ' ')
+            only_text = " ".join(only_text.split()) + ' '
+            only_text = only_text.replace('. . .', '')
+            only_text = only_text + '\n'
+            output_file.write(only_text.encode("utf-8"))
+            print(only_text)
+
+client.close()
